02baa6356f519208ea995b0f9624186185e173cf
Melichar Martin <melichar@domain.local>
2020-04-06 00:41:29 +0200

    cleaning main.c

--- main.c
+++ main.c
@@ -6,24 +6,5 @@ int main () {
 
   //cube_test(50000);
 
-//Cube * c = new_cube();
-
-//toggle_drawing(c);
-
-/*
-for (int i = 0; i < 100000; i++)
-{
-    String_guard * string_guard = get_random_commands_sequence(100);
-    execute_commands(string_guard, c);
-    solve_front_cross(c);
-    destroy_string_guard(string_guard);
-    destroy_string_guard(c->c);
-    c->c = new_string_guard();
-}
-
-destroy_cube(c);
-
-  getc(stdin);
-*/
   return 0;
 }
